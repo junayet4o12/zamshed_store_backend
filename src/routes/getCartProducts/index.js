23e4ac86e6d72d6dc49ff76a5a6cc7fe9b1d9ef9
junayet <muhammadjunayetmaruf@gmail.com>
2024-06-04 10:48:19 +0300

    get many products by ids endpoint has made

---
+++ src/routes/getCartProducts/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+const Products = require('../../modals/products/products');
+const mongoose = require('mongoose');
+var router = express.Router();
+router.post('/cartProducts', async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const ObjectIdArray = body.map(id => new mongoose.Types.ObjectId(id))
+    const result = await Products.find({ _id: { $in: ObjectIdArray } })
+    console.log(result);
+    res.send(result);
+})
+module.exports = router
