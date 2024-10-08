9e77aa28e365092dc4179a9d0fe18d4d9cce7fb5
junayet <muhammadjunayetmaruf@gmail.com>
2024-10-08 10:37:43 +0300

    changes

--- src/routes/allProperties/index.js
+++ src/routes/allProperties/index.js
@@ -3,10 +3,12 @@ const Property = require('../../modals/properties/Properties');
 var router = express.Router()
 router.post('/api/v1/properties/filter', async (req, res) => {
     try {
-        const { location, checkIn, checkout, category, adults, children, infants, pets } = req.body;
-        console.log(req.body);
-
-
+        const { location, checkIn, checkout, category, adults, children, infants, pets, price, type, roomsAndBeds, amenities } = req.body;
+        const { room, bed, bathroom } = roomsAndBeds;
+        const { wifi, kitchen, washer } = amenities;
+        let { min = 0, max = 550 } = price;
+        min = min === null ? 0 : min;
+        max = max === null ? 550 : max;
         const query = {};
 
 
@@ -24,8 +26,20 @@ router.post('/api/v1/properties/filter', async (req, res) => {
             if (infants) query['acceptableGuestAmount.infants'] = { $gte: infants };
             if (pets) query['acceptableGuestAmount.pets'] = { $gte: pets };
         }
+        query['roomsAndBeds.rooms'] = { $gte: room };
+        query['roomsAndBeds.beds'] = { $gte: bed };
+        query['roomsAndBeds.bathrooms'] = { $gte: bathroom };
 
+        if (wifi || kitchen || washer) {
+            if (wifi) query['amenities.wifi'] = true;
+            if (kitchen) query['amenities.kitchen'] = true;
+            if (washer) query['amenities.washer'] = true;
 
+        }
+        if (type && type !== 'anyType') {
+            query.typeOfPlace = type;
+        }
+        query.pricePerNight = { $gte: min, $lte: max };
         let properties = await Property.find(query);
 
         if (checkIn) {
